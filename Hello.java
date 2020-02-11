4f99854521095e876a33ce50cc65b75f7e7ab1b7
acastel <acastel@umich.edu>
2020-02-11 10:56:49 -0800

    redo

--- Hello.java
+++ Hello.java
@@ -1,11 +1,9 @@
-public class Hello
- {
-  public static void main(string[] args)
-  {
-    for(int i=1;i<=10;i++)
-    {
-      System.out.printIn("Hello World.."+1);
-    }
-  }
+public class Hello {
+	public static void main(String[] args) {
+
+		for(int i=1;i<=10;i++) {
+
+			System.out.println("Hello World..."+i);
+		}
+	}
 }
-//this is a resubmition
